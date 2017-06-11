58c16127e1c309b61104c0ccdc728e7c0ab62984
sam11385 <samluedke@gmail.com>
2017-06-11 09:01:52 -0400

    Create flickr service, components

---
+++ src/app/flickr/flickr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flickr',
+  templateUrl: './flickr.component.html',
+  styleUrls: ['./flickr.component.css']
+})
+export class FlickrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
