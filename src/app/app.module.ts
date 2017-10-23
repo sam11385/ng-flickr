2c4f461ba4e0d79d4ad52fa94ad9576a0069aae5
sam11385 <samluedke@gmail.com>
2017-10-23 14:43:38 -0400

    Upgrade to latest material design stuff, reinitiate package.json, update project info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatFormFieldModule,
+  MatInputModule,
+  MatCardModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { FlickrComponent } from './flickr/flickr.component';
-import {FlickrService} from './flickr.service';
+import { FlickrService } from './flickr.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FlickrComponent
-  ],
+  declarations: [AppComponent, FlickrComponent],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
-    MaterialModule,
-    BrowserAnimationsModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    BrowserAnimationsModule,
   ],
   providers: [FlickrService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
