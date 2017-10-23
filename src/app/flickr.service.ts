62a2e80ee4f9de506bc2799e8b8be299c5fbc4bf
sam11385 <samluedke@gmail.com>
2017-10-23 14:50:04 -0400

    Adding some styles and updates to layout

--- src/app/flickr.service.ts
+++ src/app/flickr.service.ts
@@ -7,27 +7,27 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class FlickrService {
-    result$: Observable<string[]>;
-    key = 'fe2b00007676770451fa8b4bb61bdef2';
-    constructor(private http: Http) { };
+  result$: Observable<string[]>;
+  key = 'fe2b00007676770451fa8b4bb61bdef2';
+  constructor(private http: Http) {}
 
-    getResult(query: string) {
-        const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this.key}&tags=${query}&per_page=12&format=json&nojsoncallback=1`;
-        return this.http
-            .get(url)
-            .map(res => res.json())
-            .map((val) => {
-                if (val.stat === 'ok') {
-                    return val.photos.photo.map((photo: any) => {
-                        return {
-                            url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
-                            title: photo.title
-                        };
-                    });
-                }
-                else {
-                    return [];
-                }
-            });
-    }
+  getResult(query: string) {
+    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this
+      .key}&tags=${query}&per_page=48&format=json&nojsoncallback=1`;
+    return this.http
+      .get(url)
+      .map(res => res.json())
+      .map(val => {
+        if (val.stat === 'ok') {
+          return val.photos.photo.map((photo: any) => {
+            return {
+              url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
+              title: photo.title,
+            };
+          });
+        } else {
+          return [];
+        }
+      });
+  }
 }
